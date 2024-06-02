a4411d5cc87cd4cccfde6e4c14683185d6bbf38e
Waliston <wesantosdev@gmail.com>
2024-06-02 11:15:32 -0300

    hotel back functions

---
+++ hotel_back/src/Controller/Room.Controller.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const Room = require("../Model/Room.Model");
+
+//Get A Room
+async function GetRoom(req, res) {
+  let id = req.params.id;
+  try {
+    let room = await Room.findById(id);
+    res.status(500).json(room);
+  } catch (error) {
+    res.status(500).semd({ message: error });
+  }
+}
+
+//Get All Rooms
+async function GetAllRoom(req, res) {
+  try {
+    let rooms = await Room.find();
+    res.status(200).json(rooms);
+  } catch (error) {
+    req.status(500).send({ message: error });
+  }
+}
+
+//Update Room
+async function UpdateRoom(req, res) {
+  try {
+    let updateroominformations = await Room.updateOne(
+      { _id: req.params.id },
+      {
+        $set: {
+          Name: req.body.Name,
+          Location: req.body.Location,
+          Number: req.body.Number,
+          Status: req.body.Status,
+          Description: req.body.Description,
+          PricePerNight: req.body.PricePerNight,
+          Capacity: req.body.Capacity,
+        },
+      }
+    );
+    res.status(200).json(updateroominformations);
+  } catch (error) {
+    res.status(500).send({ message: error });
+  }
+}
+
+//Delete Room
+async function DeleteRoom(req, res) {
+  let id = req.params.id;
+
+  try {
+    let delected = await Room.findByIdAndDelete(id);
+    req.status(200).send("Room Delected");
+  } catch (error) {
+    res.status(500).send({ message: error });
+  }
+}
+
+export default {
+  GetRoom,
+  GetAllRoom,
+  UpdateRoom,
+  DeleteRoom,
+};
--- hotel_back/src/Model/Room.Model.js
+++ hotel_back/src/Model/Room.Model.js
@@ -4,7 +4,10 @@ const Room = mongoose.Schema({
     Name: { type: String, default: null },
     Location: { type: Number, default: null },
     Number: { type: Number, default: null },
-    Status: { type: String, default: null }
+    Status: { type: Number, default: null },
+    Description: { type: String, default: null },
+    PricePerNight: { type: Number, default: null },
+    Capacity: { type: Number, default: null }
 })
 
 module.exports = mongoose.model("Room", Room);
